7d9551b844735767082128f959b6934aae949a8f
Kanzal Qalandri <kanzalqalandri@gmail.com>
2024-03-25 13:13:54 +0500

    v0.6

--- setup.sh
+++ setup.sh
@@ -181,6 +181,15 @@ download_and_install_font "FiraSans" "fira-sans" "https://www.1001fonts.com/down
 
 
 # ===============================
+#        Install Kubectl
+# ===============================
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+
+# ===============================
 #        Updatng Everything
 # ===============================
 
