813d32f7d2fda86d7fe95f808d6df5b3292398cf
Kanzal Qalandri <kanzalqalandri@gmail.com>
2024-03-25 13:21:52 +0500

    v0.7

--- setup.sh
+++ setup.sh
@@ -190,6 +190,15 @@ rm kubectl
 
 
 # ===============================
+#        Install Helm
+# ===============================
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+sudo ./get_helm.sh
+rm get_helm.sh
+
+# ===============================
 #        Updatng Everything
 # ===============================
 
