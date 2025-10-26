f5c85d2887c3eab6376ea92cca476128793c70a0
Kanzal Qalandri <kanzalqalandri@gmail.com>
2025-10-26 00:42:44 -0400

    add more software

---
+++ apps/flatpaks.sh
@@ -0,0 +1,44 @@
+# Planify
+flatpak install -y flathub io.github.alainm23.planify
+
+# FireFox
+flatpak install -y  flathub org.mozilla.firefox
+
+# Gopeed
+flatpak install -y flathub com.gopeed.Gopeed
+
+# Discord
+flatpak install -y  flathub com.discordapp.Discord
+
+# Flatsweap
+flatpak install -y  flathub io.github.giantpinkrobots.flatsweep
+
+# MissionCenter
+flatpak install  -y flathub io.missioncenter.MissionCenter
+
+# LocalSend
+flatpak install -y  flathub org.localsend.localsend_app
+
+# Gear Lever
+flatpak install -y flathub it.mijorus.gearlever
+
+# Errands
+flatpak install -y flathub io.github.mrvladus.List
+
+# Headlamp
+flatpak install -y flathub io.kinvolk.Headlamp
+
+# Remmina
+flatpak install -y flathub org.remmina.Remmina
+
+# Bitwaren
+flatpak install -y flathub com.bitwarden.desktop
+
+# Flameshot
+flatpak install -y flathub org.flameshot.Flameshot
+
+# Freelens
+flatpak install  -y flathub app.freelens.Freelens
+
+# Celluloid
+flatpak install -y flathub io.github.celluloid_player.Celluloid
